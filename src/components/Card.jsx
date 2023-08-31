25cebc1debf26f7c3686d8f29dfc49d9c0fb3c4b
Edixon Jose Tovar Rodriguez <erodriguez@Ufinet.com>
2023-08-31 11:10:54 -0300

    carrrito de compras

---
+++ src/components/Card.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import '../styles/card.css'
+
+
+export const Card = ({ imagen, titulo, descripcion, precio, handleAgregar, hanldeQuitar}) => {
+
+    const [added, setAdded] = useState(false)
+    const clickAgregar = () =>{
+      handleAgregar()
+      setAdded(true)
+    }
+    const clickEliminar = () =>{
+      hanldeQuitar()
+      setAdded(false)
+    }
+
+
+  return (
+
+    <div className="tarjeta container">
+      
+          
+        <img src={imagen} alt={titulo} className="tarjeta-imagen" />
+        
+        <div className="tarjeta-conenido">
+        <h3 className="tarjeta-titulo">{titulo}</h3>
+        <p className="tarjeta-descripcion">{descripcion}</p>
+        <p className="tarjeta-precio">{precio}</p>
+   {/*Este condicional es para saber si hay elementos para quitar o agregar al carrito, dependiendo lo que haya, activara un boton o ambos */}
+        {
+            added ? <button type="button" className="boton-eliminar" onClick={clickEliminar}>Agregado</button> :
+
+            <button type="button" className="boton-agregar" onClick={clickAgregar}>Agregar</button>
+        }
+        </div>
+        
+    </div>
+  )
+}
